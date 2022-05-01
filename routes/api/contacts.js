8fa50ad68d3ec6ca63461eedb97de2cbb9b299f7
galiovets <m.galiovets@gmail.com>
2022-05-01 23:17:27 +0300

    added post and put methods

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const Joi = require("joi");
 const contactsOperations = require("../../models/contacts");
 
 const router = express.Router();
+
+const contactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().min(3).max(15).required(),
+});
 
 router.get("/", async (req, res, next) => {
   try {
@@ -30,7 +42,20 @@ router.get("/:contactId", async (req, res, next) => {
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = contactSchema.validate(req.body);
+
+    if (error) {
+      res.status(400).json({
+        message: "missing required name field",
+      });
+    }
+
+    const newContact = await contactsOperations.addContact(req.body);
+    res.status(201).json(newContact);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
@@ -51,7 +76,29 @@ router.delete("/:contactId", async (req, res, next) => {
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = contactSchema.validate(req.body);
+
+    if (error) {
+      res.status(400).json({
+        message: "missing fields",
+      });
+    }
+
+    const { contactId } = req.params;
+    const updatedContact = await contactsOperations.updateContact(
+      contactId,
+      req.body
+    );
+
+    if (!updatedContact) {
+      res.status(404).json({ message: "Not found" });
+    }
+
+    res.json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
